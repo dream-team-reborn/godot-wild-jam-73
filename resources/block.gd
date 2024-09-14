51c5eb6309f2d59574d21d4b2b402c56820b6503
Matteo Stefanini <matteo.stefanini.93@gmail.com>
2024-09-14 16:15:49 +0200

    feat(resource): base resource

---
+++ resources/block.gd
@@ -0,0 +1,16 @@
+@tool
+class_name Block extends Resource
+
+@export var mesh: Mesh
+@export var cost: int
+@export var cost_interval: int
+@export var income: int
+@export var income_interval: int
+@export var population: int
+@export var population_interval: int
+@export var food: int
+@export var food_interval: int
+@export var water: int
+@export var water_interval: int
+@export var fun: int
+@export var fun_interval: int
