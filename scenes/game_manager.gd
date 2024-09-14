d23342d36ca06368a4fcb8754e1be54e322bf356
sberto <s.bertolotto92@gmail.com>
2024-09-14 19:24:50 +0200

    game manager

---
+++ scenes/game_manager.gd
@@ -0,0 +1,4 @@
+class_name GameManager extends Node
+
+enum GameState { HOME_SCREEN, UI_SELECTION, BLOCK_PLACEMENT }
+var game_state: GameState = GameState.BLOCK_PLACEMENT # TODO
--- scripts/input_handler.gd
+++ scripts/input_handler.gd
@@ -4,7 +4,15 @@ func _process(delta: float) -> void:
 	var horizontal = Input.get_axis("move_left", "move_right")
 	var vertical = Input.get_axis("move_down", "move_up")
 	
-	GlobalEventBus.publish("move", [Vector2(horizontal, vertical)])
+	match %GameManager.game_state:
+		GameManager.GameState.BLOCK_PLACEMENT:
+			var move_direction = - Vector2(vertical, horizontal)
+			move_direction = move_direction.rotated(%Camera3D.rotation.y)
+			GlobalEventBus.publish("move", [move_direction.normalized()])
+		GameManager.GameState.UI_SELECTION:
+			print("TODO")
+		GameManager.GameState.HOME_SCREEN:
+			print("TODO")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("confirm"):
