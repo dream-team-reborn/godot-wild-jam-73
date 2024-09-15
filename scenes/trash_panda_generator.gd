206678670fae753b6b09feb6fccaeda2dbff5a3b
sberto <s.bertolotto92@gmail.com>
2024-09-16 00:34:22 +0200

    raccoon animation

--- scenes/trash_panda_generator.gd
+++ scenes/trash_panda_generator.gd
@@ -8,10 +8,11 @@ func _ready() -> void:
 	for c in range(circles):
 		for i in range(raccoon_amount):
 			var raccoon = _raccoon_scene.instantiate() as Node3D
+			await get_tree().create_timer(0.001).timeout
 			add_child(raccoon)
-			raccoon.global_position.x = cos(2 * PI * i/raccoon_amount + (c))
-			raccoon.global_position.z = sin(2 * PI * i/raccoon_amount + (c))
+			raccoon.global_position.x = cos(2 * PI * i/raccoon_amount + (c/5.))
+			raccoon.global_position.z = sin(2 * PI * i/raccoon_amount + (c/5.))
 			raccoon.global_position *= 20 * sqrt(2) + (c * c + c)
 			raccoon.global_position.y = 1
-			raccoon.look_at(Vector3(0,0,0))
+			raccoon.look_at_from_position(raccoon.global_position, Vector3(0,0,0))
 			raccoon.rotate(Vector3(0,1,0), PI/2)
