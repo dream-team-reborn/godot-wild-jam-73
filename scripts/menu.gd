2d2623cb98d5df91e5b39b596cea5855b12d767c
sberto <s.bertolotto92@gmail.com>
2024-09-16 21:28:58 +0200

    options WIP

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -17,3 +17,19 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit() # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	$PopupMenu.show()
+
+
+func _on_weird_colors_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_sound_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	$PopupMenu.hide()
